a0778437defaab59220dbccba232aab57c5fe9fb
kingvirus0 <151681163+kingvirus0@users.noreply.github.com>
2025-04-19 05:37:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 require('dotenv').config(); 
 
 global.owner = process.env.OWNER_NUMBER; 
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
-global.autoTyping = process.env.AUTO_TYPING === 'true'; 
-global.autoRecord = process.env.AUTO_RECORD === 'true';
+global.anitav4 = process.env.BOT_NAME || "Virus_,V1";
+global.ownername = process.env.OWNER_NAME || "King virus";
+global.packname = process.env.PACK_NAME || "Virus_v1";
+global.author = process.env.AUTHOR || "king virus";
+global.channelname = process.env.CHANNEL_NAME || "virus";
+global.channeljid = process.env.CHANNEL_JID || "";
+global.autoTyping = process.env.AUTO_TYPING === 'false'; 
+global.autoRecord = process.env.AUTO_RECORD === 'false';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
 global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
@@ -29,25 +29,25 @@ global.roles = {
 };
 
 
-global.public = process.env.PUBLIC === 'true';
+global.public = process.env.PUBLIC === 'false';
 
 global.ANTIDELETE = process.env.ANTIDELETE === 'true';
 global.unavailable = process.env.UNAVAILABLE === 'true';
 global.available = process.env.AVAILABLE === 'true';
-global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'true';
-global.chatbot = process.env.CHATBOT === 'true';
-global.autoreact = process.env.AUTO_REACT === 'true';
+global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'false';
+global.chatbot = process.env.CHATBOT === 'false';
+global.autoreact = process.env.AUTO_REACT === 'false';
 
-global.WELCOME = process.env.WELCOME === 'true';
+global.WELCOME = process.env.WELCOME === 'false';
 
 global.prefix = process.env.PREFIX || '.';
 
-global.autobio = process.env.AUTO_BIO === 'true';
+global.autobio = process.env.AUTO_BIO === 'false';
 
-global.ANTICALL = process.env.ANTICALL === 'true';
+global.ANTICALL = process.env.ANTICALL === 'false';
 
-global.antilink = process.env.ANTILINK === 'true';
-global.antilinkkick = process.env.ANTILINK_KICK === 'true';
+global.antilink = process.env.ANTILINK === 'false';
+global.antilinkkick = process.env.ANTILINK_KICK === 'false';
 global.antilinkwarn = process.env.ANTILINK_WARN === 'true';
 
 global.mess = {
